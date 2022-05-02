254e7c0d6cb0c726054736141446c9dae08330a1
DaanKorver <daank2000@hotmail.nl>
2022-05-02 21:03:41 +0200

    tabs

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.set('view engine', 'ejs')
 app.use(compression())
 app.use(express.static(path.join(__dirname, '/public')))
 //Setting cache header to enable public 10 minute cache
-app.use((req, res, next)=>{
-  res.set('Cache-control', 'public, max-age=600')
-  next()
+app.use((req, res, next) => {
+	res.set('Cache-control', 'public, max-age=600')
+	next()
 })
 
 app.use('/', indexRoute)
@@ -25,7 +25,7 @@ app.use('/about', aboutRoute)
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  res.status(404).render('error')
+	res.status(404).render('error')
 })
 
 app.listen(PORT, () => console.log(`Listening on http://[::]:${PORT}`))
